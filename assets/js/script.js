47bad4d2cc5cd9b9c1fcf7674e755d83dede2d41
nbirckel <nbirckel@users.noreply.github.com>
2021-10-08 11:22:55 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var rep =["Ça dépend", "Vu le prix du gaz, non.", "Non, par contre tu peux mettre un pull"];
+var rep =["Ça dépend.", "Vu le prix du gaz, non.", "Non, par contre tu peux mettre un pull.","Non."];
 function newRep(){
 let numrand = Math.floor(Math.random()* rep.length);
 document.getElementById('repdisp').innerHTML = rep[numrand];
